f529bdff73f09042d059f38edbcea570854bf931
Leonardo Leonotti <l.leonotti@sidi.org.br>
2023-09-08 10:12:55 -0300

    APi calls

--- App.js
+++ App.js
@@ -1,25 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Tabs from './src/components/Tabs'
-import { View, ActivityIndicator } from 'react-native'
-
+import { View, ActivityIndicator, StyleSheet } from 'react-native'
+import * as Location from 'expo-location'
+import { WEATHER_API_KEY } from '@env'
 const Tab = createBottomTabNavigator()
 
 function App() {
   const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [weather, setWeather] = useState([])
+  const [lat, setLat] = useState([])
+  const [lon, setLon] = useState([])
+
+  const fetchWeatherData = async () => {
+    try {
+      const res =
+        await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}
+      `)
+      const data = await res.json()
+      setWeather(data)
+    } catch (e) {
+      setError('Could not fetch weather')
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setError('permission to access location denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync()
+      setLat(location.coords.latitude)
+      setLon(location.coords.longitude)
+      await fetchWeatherData()
+    })()
+  }, [lat, lon])
+
+  if (weather) {
+    console.log(weather)
+  }
+
   if (loading) {
     return (
-      <View>
+      <View style={styles.container}>
         <ActivityIndicator size={'large'} color={'blue'} />
       </View>
     )
   }
+
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
 
 export default App
